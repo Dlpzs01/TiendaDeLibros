7251cd59f9e7125d52f5d68351b5ed07b59ff8b6
DlopezS98 <01dlopezs98@gmail.com>
2023-11-03 20:33:58 -0600

    Agregando la entidad de categoria

---
+++ Entidades/Categoria.cs
@@ -0,0 +1,10 @@
+public class Categoria
+{
+    public Guid Id { get; set; }
+    public string Nombre { get; set; }  = string.Empty;
+    public string Descripcion { get; set; } = string.Empty;
+
+    void Crear() { }
+    void Eliminar() { }
+    void CrearOtraCosa() {}
+}
