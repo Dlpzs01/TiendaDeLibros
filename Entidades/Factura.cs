6f47ba738cc7a6542e38b70a6bd923a8be94cacc
DlopezS98 <01dlopezs98@gmail.com>
2023-11-03 20:46:15 -0600

    Agregando la entidad factura

---
+++ Entidades/Factura.cs
@@ -0,0 +1,7 @@
+public class Factura
+{
+    public Guid Id { get; set; }
+    public string Cliente { get; set; } = string.Empty;
+    public decimal Subtotal { get; set; }
+    public decimal Total { get; set; }  
+}
